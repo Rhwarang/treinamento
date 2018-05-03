54b08651ae0f2c64d7d739468d594d28bdc6c10c
Rodrigo <rodrigo.dealmeidadias@gmail.com>
2018-05-03 12:41:02 -0300

    Initial commit

---
+++ resources/views/cliente/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <ol class="breadcrumb panel-heading">
+                    <li class="active"> Clientes </li>
+                </ol>
+                <div class="panel-body">
+                    <p>
+                        <a class="btn btn-info" href="{{route('cliente.adicionar')}}"> Adicionar </a>
+                    </p>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>E-mail</th>
+                                <th>Endereço</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach($clientes as $cliente)
+                                <tr>
+                                    <th scope="row"> {{ $cliente->id }} </th>
+                                    <td> {{ $cliente->nome }} </td>
+                                    <td> {{ $cliente->email }} </td>
+                                    <td> {{ $cliente->endereco }} </td>
+                                    <td> 
+                                        <a class="btn btn-default" href="{{route('cliente.editar', $cliente->id)}}"> Editar </a>
+                                        <a class="btn btn-danger" href="javascript:(confirm('Deseja realmete excluir?') ? window.location.href='{{route('cliente.deletar', $cliente->id)}}' : '')"> Excluir </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <div align="center"> 
+                        {{  $clientes->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
